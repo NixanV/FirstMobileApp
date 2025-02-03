0f499d98d5967a19e71a802cac371dde2d9f3e10
NixanV <nikip1303@gmail.com>
2025-02-03 22:44:21 +0200

    just more css added

--- First-mobile-app/app/index.tsx
+++ First-mobile-app/app/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text, StyleSheet, Pressable} from 'react-native'
 import { Link } from 'expo-router';
 import style from './index.module.css';
+import { rgbaColor } from 'react-native-reanimated/lib/typescript/Colors';
 
 
 
@@ -12,14 +13,15 @@ function App() {
             <Text style={styles.text}>
                 Well just import some text here
             </Text>
-            <Link href="/explore" style={styles.link} asChild>
-                <Pressable>
-                    <Text>
+        </View>
+            <Link href="/explore" style={{marginHorizontal: 'auto'}} asChild>
+                <Pressable style={styles.buttonPres}>
+                    <Text style={styles.buttonText}>
                         Explore
                     </Text>
                 </Pressable>
             </Link>
-        </View>
+        
         </>
     )
 }
@@ -29,19 +31,34 @@ export default App
 const styles = StyleSheet.create({
     container: {
         backgroundColor: "#00ffff",
-        
+        margin: 50,
     },
     text: {
         color: "red",
         fontStyle: "italic",
         fontSize: 16,
-        fontFamily: "monospace"
+        fontFamily: "monospace",
+        margin: 20
     },
     link: {
         color: "blue",
-        textDecorationColor: "underline",
+        textDecorationLine: "underline",
         fontFamily: "Times New Roman",
         fontSize: 18,
         padding:5,
+    },
+    buttonPres:{
+        height:60,
+        borderRadius:20,
+        justifyContent: "center",
+        backgroundColor: 'rgba(0,0,0,0.75)',
+        padding:6
+    },
+    buttonText:{
+        fontSize:16,
+        color:'white',
+        fontWeight:'bold',
+        textAlign:'center'
     }
+
 })
